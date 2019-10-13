1cc59620c7996a19bff965a20c90b74a438ee32d
Zach Huxford <zachbwh@gmail.com>
2019-10-13 13:32:13 +1300

    small change to deploy script to add api key

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -5,6 +5,7 @@ if [ $TRAVIS_BRANCH == 'master' ] ; then
   eval "$(ssh-agent -s)"
   ssh-add
   npm install
+  sed -i -e "s/\$lastfm_api_key/$lastfm_api_key/g" config.js
   rsync -rq --delete $TRAVIS_BUILD_DIR/ travis@ec2-52-89-255-49.us-west-2.compute.amazonaws.com:/home/ubuntu/api.zachhuxford.io
 else
   echo "Not deploying, since this branch isn't master."
